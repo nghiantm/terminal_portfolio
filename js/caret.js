a0ab22c340a09dc8271cab71de6124bc3c27bb90
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2025-01-11 16:22:14 -0500

    init

---
+++ js/caret.js
@@ -0,0 +1,45 @@
+function $(elid) {
+    return document.getElementById(elid);
+  }
+  
+var cursor;
+window.onload = init;
+
+function init() {
+  cursor = $("cursor");
+  cursor.style.left = "0px";
+  document.getElementById("texter").focus(); // Ensure textarea gets focus on load
+}
+
+function nl2br(txt) {
+  return txt.replace(/\n/g, "").replace(/ /g, "&nbsp;"); // Replace spaces with non-breaking spaces
+}
+
+function typeIt(from, e) {
+  e = e || window.event;
+  var typer = $("typer");
+  var value = from.value;
+  console.log(value)
+  setTimeout(() => {
+    typer.innerHTML = nl2br(from.value);
+  }, 0);
+}
+
+function moveIt(count, e) {
+  e = e || window.event;
+  var keycode = e.keyCode || e.which;
+  if (keycode === 37 && parseInt(cursor.style.left) > -(count - 1) * 10) {
+    cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
+  } else if (keycode === 39 && parseInt(cursor.style.left) + 10 < count * 10) {
+      cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
+  }
+}
+
+function alert(txt) {
+  console.log(txt);
+}
+
+// Ensuring focus returns to textarea
+window.addEventListener("click", () => {
+  document.getElementById("texter").focus();
+});
---
+++ js/commands.js
@@ -0,0 +1,85 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = 'mailto:forrest@fkcodes.com';
+
+about = [
+  "<br>",
+  "Hi, I'm Matt!👋",
+  "I'm going into my 3rd year studying CS at Drexel.",
+  "I'm a software engineer who builds weird websites like this.",
+  "I enjoy roadtrip, photographing everything single thing, and gaming.",
+  "Want to get in touch? Feel free to shoot me an email.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "<a href='https://r-talk.netlify.app/' target='_blank'>rTalk</a>                AI-powered conversations for language learning <i class='icon-github'></i>",
+  "<a href='#' target='_blank'>Study Buddy</a>          Personal assistant for every students",
+  "<a href='https://tradequest.netlify.app/' target='_blank'>Trade Quest</a>          Risk-free trading platform for beginners",
+  "<a href='https://tradequest.netlify.app/' target='_blank'>Movie Recommender</a>    Personalized recommendations with collaborative filtering",
+  "<a href='#'>Personal Website</a>     Interactive web terminal for personal portfolio",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">about</span>          Who am I?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<br>',
+  '<span class="command">linkedin</span>       My LinkedIn',
+  '<span class="command">github</span>         My Github',
+  '<span class="command">website</span>        My personal website',
+  '<br>',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">photographs</span>    View some of my photographs',
+  "<br>",
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">secret</span>         Find the password',
+  "<br>",
+];
+
+banner = [
+  "         ___   ____",
+  "       /' --;^/ ,-_\\     \\ | /       ",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ",
+  "     /-/-/|o|-|\\-\\\\|\\\\                 .------..------..------..------.",
+  "      '`  ` |-|   `` '                 |M.--. ||A.--. ||T.--. ||T.--. |",
+  "            |-|                        | (\\/) || (\\/) || :/\\: || :/\\: |",
+  "            |-|O                       | :\\/: || :\\/: || (__) || (__) |",
+  "            |-(\\,__                    | '--'M|| '--'A|| '--'T|| '--'T|",
+  "         ...|-|\\--,\\_....              `------'`------'`------'`------' © 2025",
+  "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
+  "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
